77fd03a7212d3f99c63c436e2c1bbf79ef950ebc
j.dos.santos.cotta@accenture.com.br <ChJv.1804>
2023-08-15 20:43:18 -0300

    rename PlayerDetailFragment

--- app/src/main/java/com/example/nbastat_origin/ui/list_players/home/notifications/PlayerDetailFragment.kt
+++ app/src/main/java/com/example/nbastat_origin/ui/list_players/home/notifications/PlayerDetailFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.nbastat_origin.databinding.FragmentPlayerDetailsBinding
 
-class NotificationsFragment : Fragment() {
+class PlayerDetailFragment : Fragment() {
 
     private var _binding: FragmentPlayerDetailsBinding? = null
 
